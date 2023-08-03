0fc7be5146fc398d45c15f01b1fa70ac001466e4
kboss <atsugi.design@gmail.com>
2023-08-03 17:52:04 +0900

    ユーザー新規登録API作成

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
+const { PrismaClient } = require("@prisma/client");
 
 const PORT = 5000;
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
+const prisma = new PrismaClient();
+
+//新規ユーザー登録API
+app.post("/api/auth/register", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const user = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password,
+    },
+  });
+
+  return res.json({ user });
 });
 
 app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
